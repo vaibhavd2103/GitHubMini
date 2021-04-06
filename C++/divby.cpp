d586084e268106db23733ec7de84648a131010c7
vaibhavd2103 <bountydange@gmail.com>
2021-04-06 12:51:59 +0530

    done

---
+++ C++/divby.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sum = 0, n, i, j, count = 0;
+    cout << "Enter 1st number : " << endl;
+    cin >> i;
+    cout << "Enter 2nd Number : " << endl;
+    cin >> j;
+    for (n = i; n < j; n++)
+    {
+        if (n % 3 == 0 || n % 5 == 0)
+        {
+            sum = sum + n;
+            count++;
+        }
+    }
+    cout << "The Sum of Numbers divisible by 3 or 5 : " << sum << endl;
+    cout << "The total Number of elements divisible by 3 or 5 : " << count;
+    return 0;
+}
---
+++ Python/divby.py
@@ -0,0 +1,12 @@
+print("program find sum of numbers divisible by 3 or 5 from the given input range by user")
+sum = 0
+num1 = int(input("Enter 1st number"))
+num2 = int(input("Enter 2nd number "))
+list1 = []
+for i in range(num1, num2):
+    if (i % 3 == 0 or i % 5 == 0):
+        sum = sum+i
+        list1.append(i)
+print("sum of numbers divisible by 3 or 5 : ", sum)
+print("number of elements divisible by 3 or 5 : ", len(list1))
+print(list1)
