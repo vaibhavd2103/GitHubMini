ecfc3466d1a2aa58f17740b9999db372e4ac9e7d
vaibhavd2103 <bountydange@gmail.com>
2021-04-10 15:44:27 +0530

    This commit created my largest prime no python project

---
+++ Python/Largestprime.py
@@ -0,0 +1,18 @@
+def Largest_Prime_Factor(n):
+    prime_factor = 1
+    i = 2
+
+    while i <= n / i:
+        if n % i == 0:
+            prime_factor = i
+            n /= i
+        else:
+            i += 1
+
+    if prime_factor < n:
+        prime_factor = n
+
+    return prime_factor
+
+
+Largest_Prime_Factor(600851475143)
